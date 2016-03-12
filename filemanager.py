5a577cdff1a8944f9295a846cbdd8684ea9a22b2
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-03-12 14:29:52 +0330

    Change right pane structure

--- filemanager.py
+++ filemanager.py
@@ -1,26 +1,46 @@
 from PyQt4 import QtGui
 
 from PyQt4.QtCore import QDir
-from PyQt4.QtGui import QFileSystemModel
+from PyQt4.QtGui import QFileSystemModel, QHeaderView
 
-from mainForm import Ui_MainWindow
+from mainForm import Ui_mainWindow
 
 
-class FileManager(QtGui.QMainWindow, Ui_MainWindow):
+class FileManager(QtGui.QMainWindow, Ui_mainWindow):
     def __init__(self):
         super(FileManager, self).__init__()
 
         self.setupUi(self)
 
-        self.init_tree_View()
+        self.init_file_system_model()
+        self.init_left_pane()
+        self.init_right_pane()
 
-    def init_ui(self):
-        pass
+    def init_file_system_model(self):
+        # self.mFileSystemModel = QFileSystemModel(self.leftPane)
+        self.leftPaneFileModel = QFileSystemModel(self)
+        self.leftPaneFileModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
 
+        self.rightPaneFileModel = QFileSystemModel(self)
 
-    def init_tree_View(self):
-        self.mFileSystemModel = QFileSystemModel(self.treeView)
-        rootIndex = self.mFileSystemModel.setRootPath(QDir.homePath())
+    def init_left_pane(self):
+        rootIndex = self.leftPaneFileModel.setRootPath(QDir.homePath())
+        self.leftPane.setModel(self.leftPaneFileModel)
+        self.leftPane.setRootIndex(rootIndex)
 
-        self.treeView.setModel(self.mFileSystemModel)
-        self.treeView.setRootIndex(rootIndex)
+        for columnIndex in xrange(1, self.leftPaneFileModel.columnCount()):
+            self.leftPane.hideColumn(columnIndex)
+
+        self.leftPane.setHeaderHidden(True)
+        self.leftPane.resizeColumnToContents(0)
+
+    def init_right_pane(self):
+        rootIndex = self.rightPaneFileModel.setRootPath(QDir.homePath())
+
+        self.rightPane.setModel(self.rightPaneFileModel)
+        self.rightPane.setRootIndex(rootIndex)
+        self.rightPane.setStyleSheet("QTreeView::branch {  border-image: url(none.png); }")
+        self.rightPane.setExpandsOnDoubleClick(False)
+
+        self.rightPane.header().setStretchLastSection(False)
+        self.rightPane.header().setResizeMode(0, QHeaderView.Stretch)
