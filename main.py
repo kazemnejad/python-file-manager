23204c6345d92e0d076a35e97592e0d403f8d0af
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-04-16 15:23:38 +0430

    Fix X.org bug

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 from filemanager import FileManager
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_X11InitThreads)
+
     fileManager = FileManager()
     fileManager.show()
     app.exec_()
